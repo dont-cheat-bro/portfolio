da24e94f94490e0dbb5ac0377b6d4ba23bd7d87e
dont-cheat-bro <dont-cheat-bro@users.noreply.github.com>
2026-02-08 20:54:58 +0530

    Feat: Add Lenis smooth scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Hero } from './components/Hero';
 import { ProjectShowcase } from './components/ProjectShowcase';
 import { CaseStudy } from './components/CaseStudy';
 import ScrollToTop from './components/ScrollToTop';
+import { SmoothScroll } from './components/SmoothScroll';
 import { Navbar } from './components/Navbar';
 import { Footer } from './components/Footer';
 import { CustomCursor } from './components/CustomCursor';
@@ -11,6 +12,7 @@ import { About } from './components/About';
 function App() {
   return (
     <div className="bg-neutral-50 bg-dot-pattern min-h-screen text-neutral-900 selection:bg-neutral-900 selection:text-white cursor-none md:cursor-auto">
+      <SmoothScroll />
       <CustomCursor />
       <ScrollToTop />
       <Navbar />
